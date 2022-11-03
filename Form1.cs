1a469cee7171c8ced234bfadbcdda9b55fb9c5f3
user03 <gumbatoff.emil@yandex.ru>
2022-11-03 10:23:14 +0300

    Логика

--- Form1.cs
+++ Form1.cs
@@ -15,6 +15,32 @@ namespace WindowsFormsApp5
         public Form1()
         {
             InitializeComponent();
+            Console.WriteLine("Введите a");
+            double a = textbox1.ToDouble();
+            Console.WriteLine("Введите b");
+            double b = textbox2.ToDouble();
+            Console.WriteLine("Введите c");
+            double c = textbox3.ToDouble();
+            double D = Math.Sqrt((b * b) - (4 * a * c));
+
+            if (D == 0)
+            {
+                textbox4.Value="Так как дискриминант равен нулю то, квадратное уравнение имеет один действительный корень:";
+                double x = -b / (2 * a);
+                =$"x = {x}";
+            }
+            else if (D > 0)
+            {
+                double x1, x2;
+                x1 = (-b + D) / (2 * a);
+                x2 = (-b - D) / (2 * a);
+                textbox4.Value = x1;
+                textbox5.Value = x2;
+            }
+            else
+            {
+                textbox4.Value="Так как дискриминант меньше нуля, то уравнение не имеет действительных решений.";
+            }
         }
     }
 }
